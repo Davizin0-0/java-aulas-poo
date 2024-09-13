3900afa5f7f11d69ba8c0783a2d0930dcb41605f
unknown <davitavaressilva2007@gmail.com>
2024-09-13 11:34:35 -0300

    Associando valores a objeto

--- src/main/java/br/sesi/dts/poo/cabeca/Cabeca.java
+++ src/main/java/br/sesi/dts/poo/cabeca/Cabeca.java
@@ -1,8 +1,18 @@
 package br.sesi.dts.poo.cabeca;
 
 public class Cabeca {
-    public Olho olho;
+    public Olho olhoEsquerdo;
+    public Olho olhoDireito;
     public Orelha orelha;
     public Boca boca;
     public Nariz nariz;
+
+    public Cabeca(){
+        this.olhoEsquerdo = new Olho();
+        this.olhoDireito = new Olho();
+        this.orelha = new Orelha();
+        this.nariz = new Nariz();
+        this.boca = new Boca();
+
+    }
 }
